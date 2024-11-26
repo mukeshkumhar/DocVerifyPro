7b4a2ac93690f6cf27df2889e188767d2d7784cc
Codewhatyouwant <mukeshkumhar906@gmail.com>
2024-11-26 18:16:21 +0530

    fragment page update

--- app/src/main/java/com/example/docverifypro/SavedWebActivity.kt
+++ app/src/main/java/com/example/docverifypro/SavedWebActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class SavedWebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_saved_web)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
