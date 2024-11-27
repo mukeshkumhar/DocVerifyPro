0b826061c9dc606b47653ac7f70362dc302f60fd
Codewhatyouwant <mukeshkumhar906@gmail.com>
2024-11-27 16:11:00 +0530

    mukesh kumhar repo

--- app/src/main/java/com/example/docverifypro/HomeFragment.kt
+++ app/src/main/java/com/example/docverifypro/HomeFragment.kt
@@ -1,66 +1,36 @@
 package com.example.docverifypro
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.docverifypro.databinding.FragmentHome2Binding
+import android.widget.ImageButton
 import com.google.android.material.button.MaterialButton
 
 
 class HomeFragment : Fragment() {
 
+    fun onViewCreate(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
+        val resumeButton: MaterialButton = view.findViewById(R.id.resumecheck)
+        val webButton: MaterialButton = view.findViewById(R.id.webcheck)
 
-    private var _binding: FragmentHome2Binding? = null
-    private val binding get() = _binding!!
+        resumeButton.setOnClickListener{
+            val intent = Intent(requireContext(), ResumeSavedSkills::class.java)
+            startActivity(intent)
+        }
 
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        _binding = FragmentHome2Binding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return inflater.inflate(R.layout.fragment_home2, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.resumecheck.setOnClickListener {
-            val intent = Intent(requireContext(), ResumeSavedSkills::class.java)
-            startActivity(intent)
-
-        }
-        binding.webcheck.setOnClickListener {
-            val intent = Intent(requireContext(), ScanWebActivity::class.java)
-            startActivity(intent)
-        }
-        binding.qrCode.setOnClickListener{
-            val intent = Intent(requireContext(), QRCode::class.java)
-            startActivity(intent)
-        }
-        val sharedPreferences = requireContext().getSharedPreferences("user_data", Context.MODE_PRIVATE)
-        val userName = sharedPreferences.getString("user_name", null)
-
-        binding.userName.text = userName
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 
 }
